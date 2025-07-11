605ad548ec5530520483b20d03ed05568c4c29a6
KolliRamu123 <kolliramu2002@gmail.com>
2025-07-11 12:46:53 +0530

    Created components and routing (#1)
    Co-authored-by: KolliRamu <kolliramu20001@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileOneComponent } from './profile-one/profile-one.component';
+import { ProfileTwoComponent } from './profile-two/profile-two.component';
+import { ProfileThreeComponent } from './profile-three/profile-three.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'profile-one',component:ProfileOneComponent},
+  {path:'profile-two',component:ProfileTwoComponent},
+  {path:'profile-three',component:ProfileThreeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
